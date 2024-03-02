9372c18f9f8c44df110445ae5a67be1c48fb1562
unknown <elsayedelghazy20@gmail.com>
2024-03-02 14:13:15 +0200

    fourth commit

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const client_id = import.meta.env.VITE_IGDB_CLIENT_ID;
 const access_token = import.meta.env.VITE_IGDB_ACCESS_TOKEN;
-// const corsAnywhere = import.meta.env.VITE_CORS_ANYWHERE_URI;
+const corsAnywhere = import.meta.env.VITE_CORS_ANYWHERE_URI;
 const Api = import.meta.env.VITE_IGDB_API_URI;
 
 const apiRequest = async (config) => {
   try {
     const results = await axios({
       method: config.method,
-      url: `${Api}/${config.path}`,
+      url: `${corsAnywhere + Api}/${config.path}`,
       headers: {
         "Content-Type": "text/plain",
         "Client-ID": client_id,
